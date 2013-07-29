9f081c73a7e30ab710ca43e8e970a5bf75f7dff5
Josef Šimánek <josef.simanek@gmail.com>
2013-07-29 21:02:47 +0200

    Allow custom has many wrapper css class.

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -72,7 +72,7 @@ module ActiveAdmin
       end
 
       content = with_new_form_buffer do
-        template.content_tag :div, :class => "has_many #{association}" do
+        template.content_tag :div, :class => "has_many #{association} #{options[:wrapper_html_class]}" do
           form_buffers.last << template.content_tag(:h3, object.class.reflect_on_association(association).klass.model_name.human(:count => 1.1))
           inputs options, &form_block
 
